dbef14718e6185ce4482d655cde0bec07c8570de
Kevin <nait90@gmail.com>
2018-09-14 16:15:15 +0900

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import { MuiThemeProvider } from 'material-ui/styles';
+// import { MuiThemeProvider } from 'material-ui/styles';
 import MovieBrowser from './containers/MovieBrowser/MovieBrowser';
 
 class App extends Component {
